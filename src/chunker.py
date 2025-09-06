988ee01a25610398be5905c0dd134beab1d5a13e
SushanShakya <sushaanshakya88@gmail.com>
2025-09-06 09:31:13 +0545

    Feat: Update Chunker Implementation

--- src/chunker.py
+++ src/chunker.py
@@ -2,20 +2,18 @@ import re
 
 
 class Chunker:
-    def __init__(self, delimeter="r'(?<=[.?!])\s+'", chunk_size=100):
+    def __init__(self, delimeter=r"[\n]+", chunk_size=500):
         self.delimeter = delimeter
         self.chunk_size = chunk_size
 
     def chunk(self, text):
-        sentences = re.split(r"(?<=[.?!])\s+", text)
+        sentences = re.split(self.delimeter, text)
         chunks = []
         chunk = ""
         for s in sentences:
-            if len(chunk) + len(s) < self.chunk_size:
-                chunk += " " + s
-            else:
+            chunk += f" {s}"
+            if len(chunk) >= self.chunk_size:
                 chunks.append(chunk.strip())
-                chunk = s
-        if chunk:
-            chunks.append(chunk.strip())
+                chunk = ""
+
         return chunks
